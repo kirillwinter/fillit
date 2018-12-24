23fff8f7db5f0fa352b93c16e1f416f5309c2b99
Watty Ballabar <wballaba@e2r3p4.21-school.ru>
2018-12-24 16:57:04 +0300

    norm OK

--- includes/fillit.h
+++ includes/fillit.h
@@ -16,32 +16,32 @@
 
 # include "libft.h"
 
-typedef struct		s_elem
+typedef struct	s_elem
 {
 	char			axis[3][2];
 	char			linear[3];
 	int				height_positiv;
-	int		    	width;
+	int				width;
 	char			ch;
 	int				used;
 	struct s_elem	*next;
-}					t_elem;
+}				t_elem;
 
-int		ft_read_file_validate(char *filename);
-t_elem	*get_struct_figures(char *filename, int map_size);
-int		get_map_size(int tet_count);
-t_elem	*ft_new_fig();
-void	del_list(t_elem **alst);
-char	*map_creation(int map_size);
-int		get_map_size(int tet_count);
-void	ft_list_push_back(t_elem **begin_list, t_elem *new);
-int		recurs_fillit(char *map, t_elem *head, int map_size);
-int		fix_fig_on_map(char *map, t_elem *fig, int map_size, int *i);
-void	fillit(t_elem *head, int map_size);
-void	print_map(char *map, int map_size);
-void	get_positiv_height(t_elem *fig);
-void	get_width(t_elem *fig);
-void	get_coordinates_fig(char *buf, t_elem *fig);
-void	get_lin_coord(int map_size, t_elem *fig);
+int				ft_read_file_validate(char *filename);
+t_elem			*get_struct_figures(char *filename, int map_size);
+int				get_map_size(int tet_count);
+t_elem			*ft_new_fig();
+void			del_list(t_elem **alst);
+char			*map_creation(int map_size);
+int				get_map_size(int tet_count);
+void			ft_list_push_back(t_elem **begin_list, t_elem *new);
+int				recurs_fillit(char *map, t_elem *head, int map_size);
+int				fix_fig_on_map(char *map, t_elem *fig, int map_size, int *i);
+void			fillit(t_elem *head, int map_size);
+void			print_map(char *map, int map_size);
+void			get_positiv_height(t_elem *fig);
+void			get_width(t_elem *fig);
+void			get_coordinates_fig(char *buf, t_elem *fig);
+void			get_lin_coord(int map_size, t_elem *fig);
 
 #endif
